b0ac8933bf1923c649fa2cb06af73ee4afbb4bac
Evan Sarmiento <esarmien@g.harvard.edu>
2017-04-11 10:57:24 -0400

    Basic CAS works, adding a Dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:4-onbuild
+EXPOSE 3000
+RUN apt-get -y update
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var passport_cas = require('passport-cas');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
@@ -14,7 +14,28 @@ var app = express();
 
 var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
+// This will only proxy the user if the e-mail attribute matches esarmien@hmdc.harvard.edu.
+// At some point this should just use the Harvard LDAP short username since it is unchangab
+app.locals.DEST = process.env.DEST || '127.0.0.1';
+app.locals.DESTPORT = process.env.DESTPORT || 8080;
+app.locals.VALIDUSER = process.env.VALIDUSER || 'esarmien@hmdc.harvard.edu';
 app.locals.ENV_DEVELOPMENT = env == 'development';
+
+// setup passport for harvard cas
+passport.use(new passport_cas.Strategy({
+    version: 'CAS3.0',
+    ssoBaseURL: 'https://www.pin1.harvard.edu/cas',
+    serverBaseURL: 'https://backup-service.priv.hmdc.harvard.edu',
+    validateURL: '/serviceValidate',
+    serviceURL: 'https://backup-service.priv.hmdc.harvard.edu',
+}, function(login, cb) {
+    console.log('In strategy callback: ', login);
+    if (login.attributes.mail == app.locals.VALIDUSER) {
+        cb(null, login.attributes.mail);
+    } else {
+        cb(null, login, { message: 'Unknown results'});
+    }
+}));
 
 // view engine setup
 
@@ -29,9 +50,17 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-session')({secret: 'keyboard cat'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', routes);
-app.use('/users', users);
+// Authenticate
+app.use('/', passport.authenticate('cas', { failureRedirect: '/#!/not-authorized' }), (res, req) => {
+    console.log(req);
+});
+
+// app.use('/', routes);
+// app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
