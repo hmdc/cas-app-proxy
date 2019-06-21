b9fa316d05e52c9d258c322be13813ee2c1c54f8
Evan Sarmiento <esarmien@g.harvard.edu>
2019-06-21 13:27:07 -0400

    Handle none

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,4 @@
 module.exports = function({ proxying_mode, job_id }) {
-    if (proxying_mode === undefined) return undefined;
+    if (proxying_mode === undefined || proxying_mode === "none") return undefined;
     return require(`./${proxying_mode}`)(job_id);
 }
